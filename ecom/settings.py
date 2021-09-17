747f0896856b083ceaf342d676349cbd6204a2f5
Pawan kumar bamne <56109713+pawan9250@users.noreply.github.com>
2021-09-17 18:12:06 +0530

    Update settings.py

--- ecom/settings.py
+++ ecom/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-20%pr4u=g!j73l7k(t)2^!5whe2nj(rgq2e1yulw-c+pc2tb@m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['https://e-shop1-1.herokuapp.com/']
 
