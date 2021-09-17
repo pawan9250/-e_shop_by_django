ab9ee0b57741c12e57d2f5b25132d2e652ce0dc2
Pawan kumar bamne <56109713+pawan9250@users.noreply.github.com>
2021-09-17 17:44:20 +0530

    Update settings.py

--- ecom/settings.py
+++ ecom/settings.py
@@ -134,8 +134,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
