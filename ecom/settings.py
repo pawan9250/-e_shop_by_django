cc3464bf4b3b8105b1589977709a804a96ecdca5
Pawan kumar bamne <56109713+pawan9250@users.noreply.github.com>
2021-09-17 17:39:35 +0530

    Update settings.py

--- ecom/settings.py
+++ ecom/settings.py
@@ -132,6 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
