5a12827cb1d82a0d0710dcabd2d40c61096d85e7
Pawan kumar bamne <56109713+pawan9250@users.noreply.github.com>
2021-09-19 11:23:46 +0530

    Update settings.py

--- ecom/settings.py
+++ ecom/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-MEDIA_URL = "/images/products/"
+MEDIA_URL = "/imageseshop/static/uploads/products/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
