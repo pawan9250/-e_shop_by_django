a94f8ad0b5993d942f4c574658b9f077c8103e3b
Pawan kumar bamne <56109713+pawan9250@users.noreply.github.com>
2021-09-19 11:47:53 +0530

    Update urls.py

--- ecom/urls.py
+++ ecom/urls.py
@@ -21,7 +21,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("eshop.urls")),
-]
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
