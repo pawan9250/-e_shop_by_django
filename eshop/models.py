4e056b2e2b843779b20bbc9d6256593b5b9d56ce
Pawan kumar bamne <56109713+pawan9250@users.noreply.github.com>
2021-09-19 11:49:00 +0530

    Update models.py

--- eshop/models.py
+++ eshop/models.py
@@ -47,7 +47,7 @@ class Product(models.Model):
     price = models.IntegerField(default=0)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
     description = models.CharField(max_length=200, default='' , null=True , blank=True)
-    image = models.ImageField(upload_to='eshop/static/uploads/products/')
+    image = models.ImageField(upload_to='uploads/products/')
 
     def __str__(self):
         return self.name
